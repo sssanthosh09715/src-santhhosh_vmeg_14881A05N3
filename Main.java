5f410a02222330c9489dbca5cc8e4bb3a68c77d7
sssanthosh09715 <31915314+sssanthosh09715@users.noreply.github.com>
2017-09-13 14:09:29 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		Date e= new Date();
+		Student[] stude= new Student[n];
+	}
+
+}
